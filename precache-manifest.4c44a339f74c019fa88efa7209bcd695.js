c25a2a0b3107d3980f938a75499f07a89bd92fa4
Daus <mhusaini@wisc.edu>
2020-08-13 19:41:45 -0500

    Updates

---
+++ precache-manifest.4c44a339f74c019fa88efa7209bcd695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cb3ca77c3d62556391b200b951505a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "450a13ec44f70b16e67e",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "dd11781f1a0d69426bf6",
+    "url": "/static/js/2.cccd701c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.cccd701c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450a13ec44f70b16e67e",
+    "url": "/static/js/main.2180798d.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
